86a7551399f1f5be7bf94b33964760b42a2ae4d2
vinh-developer <vinh-developer@192.168.86.22>
2024-09-15 15:47:50 +0700

    remove comment in 88

--- two-pointer/88-Merge-Sorted-Array.py
+++ two-pointer/88-Merge-Sorted-Array.py
@@ -1,8 +1,5 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        \\\
-        Do not return anything, modify nums1 in-place instead.
-        \\\
         last = m + n - 1
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
