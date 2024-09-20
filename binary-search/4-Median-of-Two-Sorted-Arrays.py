c57326a8f8422135b6131eff38f0e5175a5b6c9a
Vinh Dang <quangvinh050293@gmail.com>
2024-09-20 21:43:57 +0700

    [4] [Time Beats: 89.94%] [Memory Beats: 91.81%] - LeetPush

--- binary-search/4-Median-of-Two-Sorted-Arrays.py
+++ binary-search/4-Median-of-Two-Sorted-Arrays.py
@@ -1,27 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A, B = nums1, nums2
-        total = len(nums1) + len(nums2)
-        half = total // 2
-        if len(B) < len(A):
-            A, B = B, A
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
         
-        l, r = 0, len(A) - 1
-        while True:
-            i = (l + r) // 2 # A
-            j = half - i - 2 # B
-
-            Aleft = A[i] if i >= 0 else float(\-infinity\)
-            Aright = A[i + 1] if (i + 1) < len(A) else float(\infinity\)
-            Bleft = B[j] if j >= 0 else float(\-infinity\)
-            Bright = B[j + 1] if (j + 1) < len(B) else float(\infinity\)
-
-            if Aleft <= Bright and Bleft <= Aright:
-                if total % 2:
-                    return min(Aright, Bright)
-                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
-            elif Aleft > Bright:
-                r = i - 1
+        m, n = len(nums1), len(nums2)
+        low, high = 0, m
+        
+        while low <= high:
+            partitionX = (low + high) // 2
+            partitionY = (m + n + 1) // 2 - partitionX
+            
+            maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
+            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
+            minX = float('inf') if partitionX == m else nums1[partitionX]
+            minY = float('inf') if partitionY == n else nums2[partitionY]
+            
+            if maxX <= minY and maxY <= minX:
+                if (m + n) % 2 == 0:
+                    return (max(maxX, maxY) + min(minX, minY)) / 2
+                else:
+                    return max(maxX, maxY)
+            elif maxX > minY:
+                high = partitionX - 1
             else:
-                l = i + 1
-        
+                low = partitionX + 1
