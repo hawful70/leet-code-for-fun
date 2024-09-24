3a381495bea96e4aba200870fca424a65abf71fa
Vinh Dang <quangvinh050293@gmail.com>
2024-09-24 22:44:55 +0700

    [4] [Time Beats: 92.06%] [Memory Beats: 20.98%] - LeetPush

--- binary-search/4-Median-of-Two-Sorted-Arrays.py
+++ binary-search/4-Median-of-Two-Sorted-Arrays.py
@@ -2,25 +2,26 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1) > len(nums2):
             nums1, nums2 = nums2, nums1
-        
-        m, n = len(nums1), len(nums2)
-        low, high = 0, m
-        
+        len1, len2 = len(nums1), len(nums2)
+
+        low, high = 0, len1
         while low <= high:
-            partitionX = (low + high) // 2
-            partitionY = (m + n + 1) // 2 - partitionX
+            partitionSmallArr = (low + high) // 2
+            partitionLargeArr = (len1 + len2 + 1) // 2 - partitionSmallArr
+
+            maxLeftSmall = float('-inf') if partitionSmallArr == 0 else nums1[partitionSmallArr - 1]
+            minRightSmall = float('inf') if partitionSmallArr == len1 else nums1[partitionSmallArr]
             
-            maxX = float('-inf') if partitionX == 0 else nums1[partitionX - 1]
-            maxY = float('-inf') if partitionY == 0 else nums2[partitionY - 1]
-            minX = float('inf') if partitionX == m else nums1[partitionX]
-            minY = float('inf') if partitionY == n else nums2[partitionY]
-            
-            if maxX <= minY and maxY <= minX:
-                if (m + n) % 2 == 0:
-                    return (max(maxX, maxY) + min(minX, minY)) / 2
+            maxLeftLarge = float('-inf') if partitionLargeArr == 0 else nums2[partitionLargeArr - 1]
+            minRightLarge = float('inf') if partitionLargeArr == len2 else nums2[partitionLargeArr]
+
+            if maxLeftSmall <= minRightLarge and maxLeftLarge <= minRightSmall:
+                if (len1 + len2) % 2 == 0:
+                    return (max(maxLeftSmall, maxLeftLarge) + min(minRightSmall, minRightLarge)) / 2
                 else:
-                    return max(maxX, maxY)
-            elif maxX > minY:
-                high = partitionX - 1
+                    return max(maxLeftSmall, maxLeftLarge)
+            elif maxLeftSmall > minRightLarge:
+                high = partitionSmallArr - 1
             else:
-                low = partitionX + 1
+                low = partitionSmallArr + 1
+            
