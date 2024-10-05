f0d359ad7a9127b7f3bbe9bb5ca77e41b49effc7
Vinh Dang <quangvinh050293@gmail.com>
2024-10-05 22:51:35 +0700

    [283] [Time Beats: 83.86%] [Memory Beats: 26.95%] - LeetPush

---
+++ two-pointer/283-Move-Zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        \\\
+        Do not return anything, modify nums in-place instead.
+        \\\
+        if len(nums) == 1:
+            return nums
+        found = 0
+        for i in range(1, len(nums)):
+            if nums[found] != nums[i] and nums[found] == 0:
+                nums[found], nums[i] = nums[i], nums[found]
+                found += 1
+            if nums[found] != nums[i] and nums[i] == 0:
+                found = i
+        
+            
+            
+            
