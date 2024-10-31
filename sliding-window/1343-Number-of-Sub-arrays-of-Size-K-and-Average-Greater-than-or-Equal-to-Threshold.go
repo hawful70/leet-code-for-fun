09e5487b4f02ed5aaf4fd9cd5c37e7ffd5f00bca
Vinh Dang <quangvinh050293@gmail.com>
2024-10-31 12:32:25 +0700

    [1343] [Time Beats: 100.00%] [Memory Beats: 8.64%] - LeetPush

--- sliding-window/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.go
+++ sliding-window/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.go
@@ -1,4 +1,4 @@
-func numOfSubarrays(arr []int, k int, threshold int) int {
+func numOfSubarrays2(arr []int, k int, threshold int) int {
     ans := 0
     left := 0
     for i := k - 1; i < len(arr); i++ {
@@ -12,9 +12,23 @@ func numOfSubarrays(arr []int, k int, threshold int) int {
     return ans
 }
 
+func numOfSubarrays(arr []int, k int, threshold int) int {
+    ans := 0
+    current_sum := sum_arr(0, k - 1, arr)
+    for i := 0; i < len(arr) - k + 1; i++ {
+        current_sum += arr[i + k - 1]
+        current_average := float64(current_sum) / float64(k)
+        if current_average >= float64(threshold) {
+            ans++
+        }
+        current_sum -= arr[i]
+    }
+    return ans
+}
+
 func sum_arr(left int, right int, arr []int) int {
     sum := 0
-    for i := left; i <= right; i++ {
+    for i := left; i < right; i++ {
         sum += arr[i]
     }
     return sum
