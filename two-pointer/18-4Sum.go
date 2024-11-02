2ccb366634b83cf0f39d9e011924bef62f36e275
Vinh Dang <quangvinh050293@gmail.com>
2024-11-02 23:05:37 +0700

    [18] [Time Beats: 100.00%] [Memory Beats: 37.59%] - LeetPush

--- two-pointer/18-4Sum.go
+++ two-pointer/18-4Sum.go
@@ -1,31 +1,31 @@
 func fourSum(nums []int, target int) [][]int {
+    sort.Ints(nums)
     var results [][]int
     var result []int
-
-    sort.Ints(nums)
-    backtrack(nums, 4, target, 0, len(nums) - 1, result, &results)
+    sum_up(4, 0, len(nums) - 1, nums, target, result, &results)
     return results
 }
 
-func backtrack(nums []int, N int, target int, left int, right int, result []int, results *[][]int) {
-    if right - left + 1 < N || N < 2 || target < nums[left] * N || target > nums[right] * N {
+func sum_up(N int, left int, right int, nums []int, target int, result []int, results *[][]int) {
+    if N < 2 || right - left + 1 < N || target < nums[left] * N || target > nums[right] * N {
         return
     }
 
     if N == 2 {
         for left < right {
-            s := nums[left] + nums[right]
-            if s == target {
+            current_sum := nums[left] + nums[right]
+            if current_sum == target {
                 temp := make([]int, len(result))
                 copy(temp, result)
 
                 temp = append(temp, nums[left], nums[right])
                 *results = append(*results, temp)
+
                 left++
                 for left < right && nums[left] == nums[left - 1] {
                     left++
                 }
-            } else if s > target {
+            } else if current_sum > target {
                 right--
             } else {
                 left++
@@ -38,9 +38,8 @@ func backtrack(nums []int, N int, target int, left int, right int, result []int,
             }
 
             result = append(result, nums[i])
-            backtrack(nums, N - 1, target - nums[i], i + 1, len(nums) - 1, result, results)
+            sum_up(N - 1, i + 1, right, nums, target - nums[i], result, results)
             result = result[:len(result) - 1]
-        } 
+        }
     }
 }
-
