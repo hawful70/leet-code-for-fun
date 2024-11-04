81f9675c7cb6071497347c630cf2e08a6c5e093a
Vinh Dang <quangvinh050293@gmail.com>
2024-11-04 21:47:58 +0700

    [1343] [Time Beats: 100.00%] [Memory Beats: 32.14%] - LeetPush

--- sliding-window/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.go
+++ sliding-window/1343-Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.go
@@ -1,35 +1,15 @@
-func numOfSubarrays2(arr []int, k int, threshold int) int {
-    ans := 0
-    left := 0
-    for i := k - 1; i < len(arr); i++ {
-        current_sum := sum_arr(left, i, arr)
-        current_average := float64(current_sum) / float64(k)
-        if current_average >= float64(threshold) {
-            ans++
-        }
-        left++
-    }
-    return ans
-}
-
 func numOfSubarrays(arr []int, k int, threshold int) int {
     ans := 0
-    current_sum := sum_arr(0, k - 1, arr)
-    for i := 0; i < len(arr) - k + 1; i++ {
-        current_sum += arr[i + k - 1]
-        current_average := float64(current_sum) / float64(k)
-        if current_average >= float64(threshold) {
-            ans++
+    cur_sum := 0
+    for i := 0; i < k - 1; i++ {
+        cur_sum += arr[i]
+    }
+    for i := 0; i <= len(arr) - k; i++ {
+        cur_sum += arr[i + k - 1]
+        if (cur_sum / k) >= threshold {
+            ans += 1
         }
-        current_sum -= arr[i]
+        cur_sum -= arr[i]
     }
     return ans
-}
-
-func sum_arr(left int, right int, arr []int) int {
-    sum := 0
-    for i := left; i < right; i++ {
-        sum += arr[i]
-    }
-    return sum
 }
