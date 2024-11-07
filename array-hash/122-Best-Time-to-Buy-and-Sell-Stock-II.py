a7bc67c61c7c15bb7b36fcaf28db3959beeb01ad
Vinh Dang <quangvinh050293@gmail.com>
2024-11-07 08:27:29 +0700

    [122] [Time Beats: 100.00%] [Memory Beats: 79.71%] - LeetPush

--- array-hash/122-Best-Time-to-Buy-and-Sell-Stock-II.py
+++ array-hash/122-Best-Time-to-Buy-and-Sell-Stock-II.py
@@ -1,11 +1,10 @@
-class Solution(object):
-    def maxProfit(self, prices):
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
         left, right = 0, 1
-        maxProfit = 0
         while right < len(prices):
             if prices[left] < prices[right]:
-                maxProfit += prices[right] - prices[left]
+                profit += prices[right] - prices[left]
             left += 1
             right += 1
-        
-        return maxProfit
+        return profit
