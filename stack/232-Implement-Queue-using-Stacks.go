eba3b9ce7f8714c7592195c1474755ed82bb9a10
Vinh Dang <quangvinh050293@gmail.com>
2024-11-09 16:45:10 +0700

    [232] [Time Beats: 100.00%] [Memory Beats: 22.91%] - LeetPush

--- stack/232-Implement-Queue-using-Stacks.go
+++ stack/232-Implement-Queue-using-Stacks.go
@@ -18,32 +18,27 @@ func (this *MyQueue) Push(x int)  {
 
 
 func (this *MyQueue) Pop() int {
-    if len(this.stackTwo) == 0 {
+    if len(this.stackOne) > 0 && len(this.stackTwo) == 0 {
         for i := len(this.stackOne) - 1; i >= 0; i-- {
             this.stackTwo = append(this.stackTwo, this.stackOne[i])
             this.stackOne = this.stackOne[:len(this.stackOne) - 1]
         }
     }
-    topQueue := this.stackTwo[len(this.stackTwo) - 1]
+    topElement := this.stackTwo[len(this.stackTwo) - 1]
     this.stackTwo = this.stackTwo[:len(this.stackTwo) - 1]
-    return topQueue
+    return topElement
 }
 
 
 func (this *MyQueue) Peek() int {
-    topQueue := 0
-    if len(this.stackTwo) > 0 {
-        topQueue = this.stackTwo[len(this.stackTwo) - 1]
-        return topQueue
-    } else {
+    if len(this.stackOne) > 0 && len(this.stackTwo) == 0 {
         for i := len(this.stackOne) - 1; i >= 0; i-- {
             this.stackTwo = append(this.stackTwo, this.stackOne[i])
             this.stackOne = this.stackOne[:len(this.stackOne) - 1]
         }
-        topQueue = this.stackTwo[len(this.stackTwo) - 1]
-        return topQueue
     }
-    return -1
+    topElement := this.stackTwo[len(this.stackTwo) - 1]
+    return topElement
 }
 
 
