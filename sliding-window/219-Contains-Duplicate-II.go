cd7c56b0728b2ecb26b4e9a170858b582410ad03
Vinh Dang <quangvinh050293@gmail.com>
2024-11-09 16:13:18 +0700

    [219] [Time Beats: 79.11%] [Memory Beats: 73.99%] - LeetPush

--- sliding-window/219-Contains-Duplicate-II.go
+++ sliding-window/219-Contains-Duplicate-II.go
@@ -1,15 +1,16 @@
 func containsNearbyDuplicate(nums []int, k int) bool {
-    window := make(map[int]bool)
+    duplicate_map := make(map[int]bool)
     left := 0
     for right, num := range nums {
-        if right - left > k {
-            delete(window, nums[left])
-            left++
-        }
-        if window[num] {
-            return true
-        }
-        window[num] = true
+        for duplicate_map[num] {
+            if right - left > k {
+                delete(duplicate_map, nums[left])
+                left++
+            } else {
+                return true
+            }
+        } 
+        duplicate_map[num] = true
     }
     return false
 }
