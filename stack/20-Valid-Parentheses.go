c295819f0503ef439100a89c246d935991093cde
Vinh Dang <quangvinh050293@gmail.com>
2024-11-09 17:38:07 +0700

    [20] [Time Beats: 100.00%] [Memory Beats: 12.70%] - LeetPush

--- stack/20-Valid-Parentheses.go
+++ stack/20-Valid-Parentheses.go
@@ -1,17 +1,17 @@
 func isValid(s string) bool {
     closeToOpen := map[rune]rune { ')': '(', '}': '{', ']': '[' }
-    stack := []rune{}
-
+    var stacks []rune
     for _, c := range s {
         if open, found := closeToOpen[c]; found {
-            if len(stack) > 0 && stack[len(stack) - 1] == open {
-                stack = stack[:len(stack) - 1]
+            if len(stacks) > 0 && open == stacks[len(stacks) - 1] {
+                stacks = stacks[:len(stacks) - 1]
             } else {
                 return false
             }
         } else {
-            stack = append(stack, c)
+            stacks = append(stacks, c)
         }
+        
     }
-    return len(stack) == 0
+    return len(stacks) == 0
 }
