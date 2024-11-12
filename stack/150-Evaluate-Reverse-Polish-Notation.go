16f94b3ebb1829ca348cb2c0159d810e3db171fb
Harry <quangvinh050293@gmail.com>
2024-11-12 19:18:31 +0700

    implement 150 in go

---
+++ stack/150-Evaluate-Reverse-Polish-Notation.go
@@ -0,0 +1,32 @@
+func evalRPN(tokens []string) int {
+	stacks := make([]int, 0)
+
+	for _, token := range tokens {
+		switch token {
+		case "+":
+			a := stacks[len(stacks)-1]
+			b := stacks[len(stacks)-2]
+			stacks = stacks[:len(stacks)-2]
+			stacks = append(stacks, a+b)
+		case "-":
+			a := stacks[len(stacks)-1]
+			b := stacks[len(stacks)-2]
+			stacks = stacks[:len(stacks)-2]
+			stacks = append(stacks, b-a)
+		case "*":
+			a := stacks[len(stacks)-1]
+			b := stacks[len(stacks)-2]
+			stacks = stacks[:len(stacks)-2]
+			stacks = append(stacks, a*b)
+		case "/":
+			a := stacks[len(stacks)-1]
+			b := stacks[len(stacks)-2]
+			stacks = stacks[:len(stacks)-2]
+			stacks = append(stacks, b/a)
+		default:
+			num, _ := strconv.Atoi(token)
+			stacks = append(stacks, num)
+		}
+	}
+	return stacks[len(stacks)-1]
+}
