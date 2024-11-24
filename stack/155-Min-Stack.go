03a0d40b3193348220e0ad77b68feb37f8446b13
Vinh Dang <quangvinh050293@gmail.com>
2024-11-24 22:46:32 +0700

    [155] [Time Beats: 53.61%] [Memory Beats: 27.17%] - LeetPush

--- stack/155-Min-Stack.go
+++ stack/155-Min-Stack.go
@@ -1,53 +1,48 @@
 type MinStack struct {
-    mainStack []int
+    stack []int
     minStack []int
 }
 
 
 func Constructor() MinStack {
-    return MinStack {
-        mainStack: []int{},
+    return MinStack{
+        stack: []int{},
         minStack: []int{},
     }
 }
 
 
 func (this *MinStack) Push(val int)  {
-    this.mainStack = append(this.mainStack, val)
-    if len(this.minStack) == 0 {
-        this.minStack = append(this.minStack, len(this.mainStack) - 1)
-    } else {
-        topIndexMin := this.minStack[len(this.minStack) - 1]
-        if this.mainStack[topIndexMin] > val {
-            this.minStack = append(this.minStack, len(this.mainStack) - 1)
-        }
+    this.stack = append(this.stack, val)
+    if len(this.minStack) == 0 || val <= this.minStack[len(this.minStack) - 1] {
+        this.minStack = append(this.minStack, val)
     }
 }
 
 
 func (this *MinStack) Pop()  {
-    if len(this.mainStack) > 0 && len(this.minStack) > 0 {
-        if this.minStack[len(this.minStack) - 1] == len(this.mainStack) - 1 {
-            this.minStack = this.minStack[:len(this.minStack) - 1]
-            this.mainStack = this.mainStack[:len(this.mainStack) - 1]
-        } else {
-            this.mainStack = this.mainStack[:len(this.mainStack) - 1]
-        }
-    }    
+    if this.stack[len(this.stack) - 1] == this.minStack[len(this.minStack) - 1] {
+        this.stack = this.stack[:len(this.stack) - 1]
+        this.minStack = this.minStack[:len(this.minStack) - 1]
+    } else {
+        this.stack = this.stack[:len(this.stack) - 1]
+    }
 }
 
 
 func (this *MinStack) Top() int {
-    if len(this.mainStack) > 0 {
-        return this.mainStack[len(this.mainStack) - 1]
+    if len(this.stack) > 0 {
+        return this.stack[len(this.stack) - 1]
     }
     return -1
 }
 
 
 func (this *MinStack) GetMin() int {
-    index := this.minStack[len(this.minStack) - 1]
-    return this.mainStack[index]
+    if len(this.minStack) > 0 {
+        return this.minStack[len(this.minStack) - 1]
+    }
+    return -1
 }
 
 
