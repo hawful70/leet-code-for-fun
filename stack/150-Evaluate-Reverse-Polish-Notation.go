dd51bee6049e426f943d0db6089ad7ad472eb443
Vinh Dang <quangvinh050293@gmail.com>
2024-11-25 22:57:44 +0700

    [150] [Time Beats: 100.00%] [Memory Beats: 57.73%] - LeetPush

--- stack/150-Evaluate-Reverse-Polish-Notation.go
+++ stack/150-Evaluate-Reverse-Polish-Notation.go
@@ -1,8 +1,12 @@
 func evalRPN(tokens []string) int {
-    stacks := make([]int, 0)
+    var stacks []int
+    if len(tokens) <= 1 {
+        number, _ := strconv.Atoi(tokens[0])
+        return number
+    }
 
-    for _, token := range tokens {
-        switch token {
+    for _, c := range tokens {
+        switch c {
         case \+\:
             a := stacks[len(stacks) - 1]
             b := stacks[len(stacks) - 2]
@@ -24,9 +28,9 @@ func evalRPN(tokens []string) int {
             stacks = stacks[:len(stacks) - 2]
             stacks = append(stacks, b / a)
         default:
-            num, _ := strconv.Atoi(token)
-            stacks = append(stacks, num)
-        }
+            number, _ := strconv.Atoi(c)
+            stacks = append(stacks, number)
+        } 
     }
     return stacks[len(stacks) - 1]
 }
