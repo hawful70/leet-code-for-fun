5521afe28946a7f2ff794961bebec06d97536a54
Vinh Dang <quangvinh050293@gmail.com>
2024-12-03 23:20:36 +0700

    [1888] [Time Beats: 50.00%] [Memory Beats: 20.00%] - LeetPush

--- sliding-window/1888-Minimum-Number-of-Flips-to-Make-the-Binary-String-Alternating.go
+++ sliding-window/1888-Minimum-Number-of-Flips-to-Make-the-Binary-String-Alternating.go
@@ -4,13 +4,14 @@ func minFlips(s string) int {
 	window_size := n
 
 	// Extend the string for odd-length cases
-	if window_size%2 != 0 {
+	if window_size % 2 != 0 {
 		s = s + s
 	}
 
 	dif1, dif2 := 0, 0
 	left := 0
 
+    fmt.Println("window_size", window_size)
 	for right := 0; right < len(s); right++ {
 		// Expected values for alternating patterns
 		expected1 := byte('0' + (right % 2)) // '0' if even index, '1' if odd index
@@ -23,10 +24,9 @@ func minFlips(s string) int {
 		if s[right] != expected2 {
 			dif2++
 		}
-
+        
 		// Check if the window size is valid
 		if right-left+1 == window_size {
-			
             minFlip = min(minFlip, dif1, dif2)
 			if s[left] != byte('0'+(left%2)) {
 				dif1--
@@ -34,6 +34,7 @@ func minFlips(s string) int {
 			if s[left] != byte('1'-(left%2)) {
 				dif2--
 			}
+
 			left++
 		}
 	}
