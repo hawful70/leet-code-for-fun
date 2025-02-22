4d9e8908943789c7f2ff06b8acd2c09fd0beaed4
Harry <quangvinh050293@gmail.com>
2025-02-22 19:05:07 +0700

    27.Remove Element in Go

---
+++ array-hash/27-Remove-Element.go
@@ -0,0 +1,24 @@
+func removeElement(nums []int, val int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left] == val {
+			if nums[left] != nums[right] {
+				nums[left], nums[right] = nums[right], nums[left]
+				left++
+			}
+			right--
+		} else {
+			left++
+		}
+	}
+
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			k++
+		} else {
+			break
+		}
+	}
+	return k
+}
