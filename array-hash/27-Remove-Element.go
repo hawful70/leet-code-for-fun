c4c33c4b6481c69194ab0f4d658177232019008e
Harry <quangvinh050293@gmail.com>
2025-02-22 19:09:21 +0700

    another solution for 27.Remove Element

--- array-hash/27-Remove-Element.go
+++ array-hash/27-Remove-Element.go
@@ -22,3 +22,15 @@ func removeElement(nums []int, val int) int {
 	}
 	return k
 }
+
+// another solution
+func removeElement(nums []int, val int) int {
+	k := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
+}
